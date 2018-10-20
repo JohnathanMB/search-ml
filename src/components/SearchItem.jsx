6fdbe2455a0078612755354cc8b264fcbd31bf1a
Julio45andres <julian.munozm@udea.edu.co>
2018-10-19 20:56:14 -0500

    Punto decimal añadido

--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import '../stylesheets/SearchItem.css'
+import React from "react";
+import "../stylesheets/SearchItem.css";
 
-export default function SearchItem({id, title, price, thumbnail}){
-    return (
-        <div class="card card-4">
-            <p>{id}</p>
-            <p>{title}</p>
-            <p>{price}</p>
-            <img src={thumbnail} alt="Item thumbnail"/>
-        </div>
-    );
+export default function SearchItem({ id, title, price, thumbnail }) {
+  return (
+    <div class="card card-4">
+      <p>{id}</p>
+      <p>{title}</p>
+      <p>${price.toLocaleString("de-DE")}</p>
+      <img src={thumbnail} alt="Item thumbnail" />
+    </div>
+  );
 }
