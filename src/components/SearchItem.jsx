591e3b22482215c26febe3453b919450025122ce
Julio45andres <julian.munozm@udea.edu.co>
2018-10-20 22:26:48 -0500

    Limpieza de comments

--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -10,27 +10,3 @@ export default function SearchItem({ title, price, thumbnail }) {
     </div>
   );
 }
-
-// class SearchItem extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       id: props.id,
-//       title: props.title,
-//       price: props.price,
-//       thumbnail: props.thumbnail
-//     }
-//   }
-//   render() {
-//     return (
-//       <div class="card card-4">
-//         {/* <p>{id}</p> */}
-//         <p>{title}</p>
-//         <p>${price.toLocaleString("de-DE")}</p>
-//         <img src={thumbnail} alt="Item thumbnail" />
-//       </div>
-//     );
-//   }
-// }
-
-// export default SearchItem;
