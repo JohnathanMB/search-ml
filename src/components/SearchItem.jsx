f96cfb6c551fccc36f3d22f9d41389f3b0ce405c
Julio45andres <julian.munozm@udea.edu.co>
2018-10-20 21:23:07 -0500

    refactor preliminar

--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "../stylesheets/SearchItem.css";
 
-export default function SearchItem({ id, title, price, thumbnail }) {
+export default function SearchItem({ title, price, thumbnail }) {
   return (
-    <div class="card card-4">
-      {/* <p>{id}</p> */}
-      <p>{title}</p>
+    <div className="card card-4">
+      <h5>{title}</h5>
       <p>${price.toLocaleString("de-DE")}</p>
       <img src={thumbnail} alt="Item thumbnail" />
     </div>
